a1593abdde603cb9e66720c884d8f47d659ad613
nelson <nelsonobuya19@gmail.com>
2023-11-30 14:28:59 +0300

    Completing the functionality of the login button and styling the login form

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,13 @@ const searchButton = document.getElementById('search-button'),
 searchClose = document.getElementById('search-close'),
 searchContent = document.getElementById('search-content')
 
+/*===============SHOW SEARCH ===============*/
 if(searchButton){
     searchButton.addEventListener('click', () => {
         searchContent.classList.add('show-search')
     })
 }
+/*=============== HIDESEARCH ===============*/
 if(searchClose){
     searchClose.addEventListener('click', ()=> {
         searchContent.classList.remove('show-search')
@@ -16,6 +18,22 @@ if(searchClose){
 
 
 /*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button'),
+loginClose = document.getElementById('login-close'),
+loginContent = document.getElementById('login-content')
+
+/*===============SHOW SEARCH ===============*/
+if(loginButton){
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
+/*=============== HIDESEARCH ===============*/
+if(loginClose){
+    loginClose.addEventListener('click', ()=> {
+        loginContent.classList.remove('show-login')
+    })
+}
 
 
 /*=============== ADD SHADOW HEADER ===============*/
